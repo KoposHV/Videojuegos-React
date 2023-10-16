b768224757b8a1b5e8a22dad55bb56cfc850375b
Kopos <carlosh.viveros12@gmail.com>
2023-10-16 14:07:06 +0200

    videojuegos

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import NotFound from './pages/NotFound/NotFound.jsx';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <BrowserRouter basename="/">
--- src/pages/Memoria/Memoria.jsx
+++ src/pages/Memoria/Memoria.jsx
@@ -1,10 +1,62 @@
 import "./Memoria.css";
+import React, { useState, useEffect } from 'react';
 
+const shuffleArray = (array) => {
+  let shuffledArray = array.slice();
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
+};
 
 const Memoria = () => {
+  const [cards, setCards] = useState([
+    '🐱', '🐶', '🐰', '🐻', '🐼', '🐨', '🦁', '🐯', '🐷', '🐵',
+    '🦊', '🐸', '🐙', '🦑', '🦐', '🦀', '🐡', '🐢', '🦕', '🐊'
+  ]);
+
+  const [flippedIndices, setFlippedIndices] = useState([]);
+  const [matchedPairs, setMatchedPairs] = useState([]);
+
+  useEffect(() => {
+    if (flippedIndices.length === 2) {
+      const [firstIndex, secondIndex] = flippedIndices;
+      if (cards[firstIndex] === cards[secondIndex]) {
+        setMatchedPairs([...matchedPairs, cards[firstIndex]]);
+      }
+      setTimeout(() => setFlippedIndices([]), 1000);
+    }
+  }, [flippedIndices, cards, matchedPairs]);
+
+  const handleCardClick = (index) => {
+    if (flippedIndices.length < 2 && !flippedIndices.includes(index)) {
+      setFlippedIndices([...flippedIndices, index]);
+    }
+  };
+
+  const shuffledCards = shuffleArray(cards);
+
   return (
-    <main>Memoria</main>
-  )
-}
+    <div className="App">
+      <h1>Juego de Memoria</h1>
+      <div className="card-container">
+        {shuffledCards.map((card, index) => {
+          const isFlipped = flippedIndices.includes(index) || matchedPairs.includes(card);
+          return (
+            <div
+              key={index}
+              className={`card ${isFlipped ? 'flipped' : ''}`}
+              onClick={() => handleCardClick(index)}
+            >
+              {isFlipped ? card : '❓'}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
 
 export default Memoria
