6f77dd379f045163d781ff13ef2511fabdb7e459
Kopos <carlosh.viveros12@gmail.com>
2023-10-18 19:53:49 +0200

    videojuegos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
+import { Outlet } from "react-router-dom";
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import { Outlet } from "react-router-dom";
+
 
 const App = () => {
   
